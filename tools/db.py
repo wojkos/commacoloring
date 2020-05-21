4bd37aec4996a24f50bb6e03ea541a13e772a830
wojtek <wojtek.kostanski@gmail.com>
2020-05-21 18:49:53 +0200

    fix url parser lib

--- tools/db.py
+++ tools/db.py
@@ -1,10 +1,13 @@
 # connect to the database
 import os
 import psycopg2
-import urlparse
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
 
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["HEROKU_POSTGRESQL_ORANGE_URL"])
+url = urlparse.urlparse('postgres://lwnowhnbiirony:5c51d45a0147b4fa774399eab8c4d1557391a086de54ae7981188680378e2fc5@ec2-54-247-89-181.eu-west-1.compute.amazonaws.com:5432/d79o3rondn8gpr')
 
 conn = psycopg2.connect(
     database=url.path[1:],
